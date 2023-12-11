6788027201c7b7e02b4d75808415168eac3da69a
Alvaro Bartolome <alvaro@argilla.io>
2023-12-11 15:47:54 +0100

    Add `v1.1/` configuration and adapted script

---
+++ v1.1/fine-tune/run_dpo.py
@@ -0,0 +1,294 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# WARNING: this script has been adapted from the original `run_dpo.py` at
+# https://github.com/huggingface/alignment-handbook/blob/main/scripts/run_dpo.py
+
+import logging
+import re
+import sys
+from typing import Any, Dict
+from datetime import timedelta
+
+import torch
+import transformers
+from transformers import AutoModelForCausalLM, PreTrainedTokenizer, set_seed
+
+from accelerate import Accelerator, InitProcessGroupKwargs
+from alignment import (
+    DataArguments,
+    DPOConfig,
+    H4ArgumentParser,
+    ModelArguments,
+    get_datasets,
+    get_kbit_device_map,
+    get_peft_config,
+    get_quantization_config,
+    get_tokenizer,
+    is_adapter_model,
+)
+from datasets import DatasetDict
+from peft import PeftConfig, PeftModel
+from trl import DPOTrainer
+
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    parser = H4ArgumentParser((ModelArguments, DataArguments, DPOConfig))
+    model_args, data_args, training_args = parser.parse()
+
+    #######
+    # Setup
+    #######
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+    log_level = training_args.get_process_log_level()
+    logger.setLevel(log_level)
+    transformers.utils.logging.set_verbosity(log_level)
+    transformers.utils.logging.enable_default_handler()
+    transformers.utils.logging.enable_explicit_format()
+
+    # Log on each process the small summary:
+    logger.info(f"Model parameters {model_args}")
+    logger.info(f"Data parameters {data_args}")
+    logger.info(f"Training/evaluation parameters {training_args}")
+
+    # Set seed for reproducibility
+    set_seed(training_args.seed)
+
+    # Increase distributed timeout to 3h to enable push to Hub to complete
+    accelerator = Accelerator(
+        kwargs_handlers=[InitProcessGroupKwargs(timeout=timedelta(seconds=6 * 1800))]
+    )
+
+    ###############
+    # Load datasets (modified)
+    ###############
+    raw_dataset = get_datasets(
+        data_args, splits=data_args.dataset_splits
+    )  # TODO: remove dep with `get_datasets`
+    split_dataset = raw_dataset["train"].train_test_split(test_size=5000, seed=training_args.seed, shuffle=True)  # type: ignore
+
+    dataset = DatasetDict(
+        {"train": split_dataset["train"], "test": split_dataset["test"]}
+    )
+
+    logger.info(
+        f"Training on the following splits: {[split + ' : ' + str(dset.num_rows) for split, dset in dataset.items()]}"
+    )
+    column_names = list(dataset["train"].features)
+    logger.info(f"Column names: {column_names}")
+
+    ###########################
+    # Data preparation function (modified)
+    ###########################
+    def apply_chat_template_and_prepare_for_dpo(
+        example: Dict[str, Any], tokenizer: PreTrainedTokenizer
+    ) -> Dict[str, Any]:
+        """This is a modified function from the original `alignment.data.apply_chat_template` to suit not just the DPO task
+        but also any dataset produced by `distilabel`."""
+        prompt = [
+            {
+                "role": "system",
+                "content": "",
+            },
+            {
+                "role": "user",
+                "content": example["instruction"],
+            },
+        ]
+        chosen = [
+            {
+                "role": "assistant",
+                "content": example["chosen_response"],
+            }
+        ]
+        rejected = [
+            {
+                "role": "assistant",
+                "content": example["rejected_response"],
+            }
+        ]
+
+        def _strip_prefix(pattern: str, text: str) -> str:
+            # Use re.escape to escape any special characters in the pattern
+            return re.sub(f"^{re.escape(pattern)}", "", text)
+
+        example["chosen"] = _strip_prefix(
+            "<|assistant|>\n", tokenizer.apply_chat_template(chosen, tokenize=False)
+        )
+        example["rejected"] = _strip_prefix(
+            "<|assistant|>\n", tokenizer.apply_chat_template(rejected, tokenize=False)
+        )
+        example["prompt"] = tokenizer.apply_chat_template(
+            prompt, tokenize=False, add_generation_prompt=True
+        )
+        return example
+
+    #####################################
+    # Load tokenizer and process datasets
+    #####################################
+    data_args.truncation_side = (
+        "left"  # Truncate from left to ensure we don't lose labels in final turn
+    )
+    tokenizer = get_tokenizer(model_args, data_args)
+
+    #####################
+    # Apply chat template (modified)
+    #####################
+    dataset = dataset.map(
+        apply_chat_template_and_prepare_for_dpo,
+        fn_kwargs={"tokenizer": tokenizer},
+        num_proc=data_args.preprocessing_num_workers,
+        remove_columns=column_names,
+        desc="Formatting comparisons with prompt template",
+    )
+
+    torch_dtype = (
+        model_args.torch_dtype
+        if model_args.torch_dtype in ["auto", None]
+        else getattr(torch, model_args.torch_dtype)
+    )
+    model_kwargs = dict(
+        revision=model_args.model_revision,
+        trust_remote_code=model_args.trust_remote_code,
+        use_flash_attention_2=model_args.use_flash_attention_2,
+        torch_dtype=torch_dtype,
+        use_cache=False if training_args.gradient_checkpointing else True,
+        device_map=get_kbit_device_map(),
+        quantization_config=get_quantization_config(model_args),
+    )
+
+    model = model_args.model_name_or_path
+    if is_adapter_model(model, model_args.model_revision):
+        # load the model, merge the adapter weights and unload the adapter
+        # Note: to run QLora, you will need to merge the based model separately as the merged model in 16bit
+        logger.info(f"Merging peft adapters for {model_args.model_name_or_path=}")
+
+        peft_config = PeftConfig.from_pretrained(
+            model_args.model_name_or_path, revision=model_args.model_revision
+        )
+
+        model_kwargs = dict(
+            revision=model_args.base_model_revision,
+            trust_remote_code=model_args.trust_remote_code,
+            use_flash_attention_2=model_args.use_flash_attention_2,
+            torch_dtype=torch_dtype,
+            use_cache=False if training_args.gradient_checkpointing else True,
+        )
+        base_model = AutoModelForCausalLM.from_pretrained(
+            peft_config.base_model_name_or_path,
+            **model_kwargs,
+        )
+        model = PeftModel.from_pretrained(
+            base_model,
+            model_args.model_name_or_path,
+            revision=model_args.model_revision,
+        )
+        model.eval()
+        model = model.merge_and_unload()
+        model_kwargs = None
+
+    ref_model = model
+    ref_model_kwargs = model_kwargs
+
+    if model_args.use_peft is True:
+        ref_model = None
+        ref_model_kwargs = None
+
+    #########################
+    # Instantiate DPO trainer
+    #########################
+    dpo_trainer = DPOTrainer(
+        model,
+        ref_model,
+        model_init_kwargs=model_kwargs,
+        ref_model_init_kwargs=ref_model_kwargs,
+        args=training_args,
+        beta=training_args.beta,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["test"],
+        tokenizer=tokenizer,
+        max_length=training_args.max_length,
+        max_prompt_length=training_args.max_prompt_length,
+        peft_config=get_peft_config(model_args),
+    )
+
+    ###############
+    # Training loop
+    ###############
+    train_result = dpo_trainer.train()
+    metrics = train_result.metrics
+    max_train_samples = (
+        data_args.max_train_samples
+        if data_args.max_train_samples is not None
+        else len(dataset["train"])
+    )
+    metrics["train_samples"] = min(max_train_samples, len(dataset["train"]))
+    dpo_trainer.log_metrics("train", metrics)
+    dpo_trainer.save_metrics("train", metrics)
+    dpo_trainer.save_state()
+
+    logger.info("*** Training complete ***")
+
+    ##########
+    # Evaluate
+    ##########
+    if training_args.do_eval:
+        logger.info("*** Evaluate ***")
+        metrics = dpo_trainer.evaluate()
+        max_eval_samples = (
+            data_args.max_eval_samples
+            if data_args.max_eval_samples is not None
+            else len(dataset["test"])
+        )
+        metrics["eval_samples"] = min(max_eval_samples, len(dataset["test"]))
+        dpo_trainer.log_metrics("eval", metrics)
+        dpo_trainer.save_metrics("eval", metrics)
+
+    ##################################
+    # Save model and create model card
+    ##################################
+    dpo_trainer.save_model(training_args.output_dir)
+    # Save everything else on main process
+    if accelerator.is_main_process:
+        kwargs = {
+            "finetuned_from": model_args.model_name_or_path,
+            "dataset": list(data_args.dataset_mixer.keys()),
+            "dataset_tags": list(data_args.dataset_mixer.keys()),
+            "tags": ["alignment-handbook"],
+        }
+        dpo_trainer.create_model_card(**kwargs)
+        # Restore k,v cache for fast inference
+        dpo_trainer.model.config.use_cache = True
+        dpo_trainer.model.config.save_pretrained(training_args.output_dir)
+        if training_args.push_to_hub is True:
+            dpo_trainer.push_to_hub()
+
+    # Ensure we don't timeout on model save / push to Hub
+    logger.info("*** Waiting for all processes to finish ***")
+    accelerator.wait_for_everyone()
+
+    logger.info("*** Run complete! ***")
+
+
+if __name__ == "__main__":
+    main()
